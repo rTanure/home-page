5a2c530992b7b0b045354a2537aedf48e5c0cfef
unknown <richardy.tanure@hotmail.com>
2023-08-19 03:02:47 -0300

    update database

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -6,7 +6,7 @@ const data = {
       time: 3,
       instructor: "Mayk Brito",
       conclusion: 2022,
-      certificate: "/certificates/discover-conectar-certificate.pdf"
+      certificate_url: "https://drive.google.com/file/d/1butfAjGS32JpBw1HuOp4DWW30PQYjkci/view?usp=drive_link"
     },
     {
       title: "Discover - fundamentar",
@@ -14,40 +14,93 @@ const data = {
       time: 28,
       instructor: "Mayk Brito",
       conclusion: 2022,
-      certificate: "/certificates/discover-fundamentar-certificate.pdf"
+      certificate_url: "https://drive.google.com/file/d/16i1f9yn12_WkUMQg6skuzMfh6nsSUQdH/view?usp=drive_link"
     },
+    {
+      title: "Discover - Especializar",
+      school: "Rocket Seat",
+      time: 11,
+      instructor: "Mayk Brito",
+      conclusion: 2022,
+      certificate_url: "https://drive.google.com/file/d/1TSzSfZeJZRf7mh8dRnDWuzrqcVDlOV-i/view?usp=drive_link"
+    },
+    {
+      title: "Javascript Completo",
+      school: "DankiCode",
+      time: 14,
+      instructor: "Gilherme Grillo",
+      conclusion: 2022,
+      certificate_url: "https://drive.google.com/file/d/1ht3r9C1h3JgzPTunFhgxnWV8QQeFuL8p/view?usp=drive_link"
+    },
+    {
+      title: "Front-End Completo",
+      school: "DankiCode",
+      time: 10,
+      instructor: "Gilherme Grillo",
+      conclusion: 2022,
+      certificate_url: "https://drive.google.com/file/d/1mDxTgfmXuGGd-NNhrJXxxSN8Y-X6eLBC/view?usp=drive_link"
+    },
+    {
+      title: "Nodejs",
+      school: "DankiCode",
+      time: 8,
+      instructor: "Gilherme Grillo",
+      conclusion: 2022,
+      certificate_url: "https://drive.google.com/file/d/1fdhQnG8aXvJIBM6HeyJGjoW1XFOdDhu4/view?usp=drive_link"
+    },
+    {
+      title: "Banco de Dados",
+      school: "DankiCode",
+      time: 5,
+      instructor: "Gilherme Grillo",
+      conclusion: 2022,
+      certificate_url: "https://drive.google.com/file/d/11jJEOff6ZbxCjPkeSTktMtAXN5_uU-SS/view?usp=drive_link"
+    },
+    // {
+    //   title: "",
+    //   school: "",
+    //   time: 0,
+    //   instructor: "",
+    //   conclusion: 9999,
+    //   certificate_url: ""
+    // },
   ],
   techs: [
-    // {
-    //   name: 'Python',
-    //   id: 'react',
-    //   description: 'Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe clara e legível. Com sua vasta biblioteca padrão e comunidade ativa, é uma escolha popular para desenvolvimento de software, análise de dados e automação de tarefas.'
-    // },
-    // {
-    //   name: 'Git',
-    //   id: 'react',
-    //   description: 'Git é um sistema de controle de versão distribuído amplamente utilizado para gerenciar o histórico de alterações em projetos de software. Ele permite que múltiplos desenvolvedores trabalhem simultaneamente em um projeto, facilitando a colaboração e o rastreamento das alterações.'
-    // },
+    {
+      name: 'Python',
+      id: 'react',
+      icon_class: "fa-brands fa-python",
+      description: 'Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe clara e legível. Com sua vasta biblioteca padrão e comunidade ativa, é uma escolha popular para desenvolvimento de software, análise de dados e automação de tarefas.'
+    },
+    {
+      name: 'Git',
+      id: 'react',
+      icon_class: "fa-brands fa-github",
+      description: 'Git é um sistema de controle de versão distribuído amplamente utilizado para gerenciar o histórico de alterações em projetos de software. Ele permite que múltiplos desenvolvedores trabalhem simultaneamente em um projeto, facilitando a colaboração e o rastreamento das alterações.'
+    },
     // {
     //   name: 'TypeScript',
     //   id: 'react',
     //   description: 'TypeScript é uma linguagem de programação desenvolvida pela Microsoft que se baseia no JavaScript, mas adiciona recursos de tipagem estática. Com o TypeScript, é possível escrever um código mais robusto e escalável, detectando erros de digitação e fornecendo recursos avançados de autocompletar.'
     // },
-    // {
-    //   name: 'JavaScript',
-    //   id: 'react',
-    //   description: 'JavaScript é uma linguagem de programação interpretada que permite adicionar interatividade e dinamismo a páginas da web. É amplamente utilizado para desenvolver funcionalidades como animações, validação de formulários e interações com o usuário.'
-    // },
-    // {
-    //   name: 'CSS 3',
-    //   id: 'react',
-    //   description: 'CSS (Cascading Style Sheets) é uma linguagem de estilo utilizada para definir a aparência e o layout de elementos em páginas da web. Com o CSS, é possível controlar cores, fontes, espaçamentos, alinhamentos e outros aspectos visuais.'
-    // },
-    // {
-    //   name: 'HTML 5',
-    //   id: 'react',
-    //   description: 'HTML (HyperText Markup Language) é uma linguagem de marcação utilizada para criar a estrutura e o conteúdo de páginas da web. Através de tags e elementos, o HTML permite definir a organização e o formato do texto, inserir imagens, links, vídeos e outros elementos interativos, permitindo assim a criação de páginas web visualmente atrativas e funcionais.'
-    // },
+    {
+      name: 'JavaScript',
+      id: 'react',
+      icon_class: "fa-brands fa-square-js",
+      description: 'JavaScript é uma linguagem de programação interpretada que permite adicionar interatividade e dinamismo a páginas da web. É amplamente utilizado para desenvolver funcionalidades como animações, validação de formulários e interações com o usuário.'
+    },
+    {
+      name: 'CSS 3',
+      id: 'react',
+      icon_class: "fa-brands fa-css3",
+      description: 'CSS (Cascading Style Sheets) é uma linguagem de estilo utilizada para definir a aparência e o layout de elementos em páginas da web. Com o CSS, é possível controlar cores, fontes, espaçamentos, alinhamentos e outros aspectos visuais.'
+    },
+    {
+      name: 'HTML 5',
+      id: 'react',
+      icon_class: "fa-brands fa-html5",
+      description: 'HTML (HyperText Markup Language) é uma linguagem de marcação utilizada para criar a estrutura e o conteúdo de páginas da web. Através de tags e elementos, o HTML permite definir a organização e o formato do texto, inserir imagens, links, vídeos e outros elementos interativos, permitindo assim a criação de páginas web visualmente atrativas e funcionais.'
+    },
     {
       name: 'React.js',
       id: 'react',
@@ -66,12 +119,12 @@ const data = {
       icon_class: 'fa-brands fa-java',
       description: 'Java é uma linguagem de programação popular, conhecida por sua portabilidade e robustez. Ela é usada amplamente no desenvolvimento de aplicativos, desde aplicativos móveis a sistemas empresariais.'
     },
-    {
-      name: 'SQL',
-      id: 'sql',
-      icon_class: 'fa-solid fa-database',
-      description: 'SQL (Structured Query Language) é uma linguagem de programação utilizada para gerenciar e manipular bancos de dados relacionais. Com o SQL, é possível realizar consultas, inserções, atualizações e exclusões de dados de forma eficiente e organizada, fornecendo uma base sólida para o armazenamento e recuperação de informações em sistemas de gerenciamento de banco de dados.'
-    },
+    // {
+    //   name: 'SQL',
+    //   id: 'sql',
+    //   icon_class: 'fa-solid fa-database',
+    //   description: 'SQL (Structured Query Language) é uma linguagem de programação utilizada para gerenciar e manipular bancos de dados relacionais. Com o SQL, é possível realizar consultas, inserções, atualizações e exclusões de dados de forma eficiente e organizada, fornecendo uma base sólida para o armazenamento e recuperação de informações em sistemas de gerenciamento de banco de dados.'
+    // },
   ],
   social: {
     instagram_url: "https://www.instagram.com/richardy.tanure/",
